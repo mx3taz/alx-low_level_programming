a703dffa2830428cfbc2d1403515f98002e78218
mx3tez <mx3tez@gmail.com>
2023-10-05 23:00:41 +0000

    Prints a sentence with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-/*
-* main - Entry point of the program
-*
-* Description: This function prints a sentence using
-* printf
-*
-* Return: Always 0 (Success)
-*/
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a sentence using printf.
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
