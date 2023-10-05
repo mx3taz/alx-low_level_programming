1b44ecce02b9bfbaf0bbf37e97bb15823646b4df
mx3tez <mx3tez@gmail.com>
2023-10-05 23:34:00 +0000

    Prints a specific sentence to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints exactly a specific sentence
+ * to the standard error.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	and that piece of art is useful" - Dora Korpar, 2015-10-19
+	return (1);
+}
