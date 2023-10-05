295cad4367d10a1b46da37da8a3921cf78bd41d7
mx3tez <mx3tez@gmail.com>
2023-10-05 23:07:56 +0000

    Prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types on the computer.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char:", sizeof(char));
+	printf("Size of an int:", sizeof(int));
+	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a long long int:", sizeof(long long int));
+	printf("Size of a float:", sizeof(float));
+
+	return (0);
+}
