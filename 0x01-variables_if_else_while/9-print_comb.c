5af5d25d3fe34f6ba4d8a825d95513a544de23ad
mx3tez <mx3tez@gmail.com>
2023-10-06 21:43:21 +0000

    Prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,9 +15,11 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(',');
-		if (i <= 57)
+		if (i != 57)
+		{
+			putchar(' ');
 			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
