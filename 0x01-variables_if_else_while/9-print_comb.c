50e98c0945274b493cfe3637a3ffe6ccb1d8ca8b
mx3tez <mx3tez@gmail.com>
2023-10-06 21:36:56 +0000

    Prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		if (i == 57)
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
