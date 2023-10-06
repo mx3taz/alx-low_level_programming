5906fdc58636c0a45a0d36521dfbefc6c7e4fbb7
mx3tez <mx3tez@gmail.com>
2023-10-06 20:55:17 +0000

    Prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: Prints all single digit numbers of base 10 starting from 0.
- * 
+ *
  * Return: Always 0 (success)
  */
 
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 		putchar(i);
 	return (0);
 }
