f1b08c03c6099618e7d3761bdc09c3ec6a0aafb9
mx3tez <mx3tez@gmail.com>
2023-10-10 21:35:59 +0000

    Prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char alphabets = 'a';
+	char alphabets;
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (; alphabets <= 'z'; alphabets++)
+		for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 			_putchar(alphabets);
 		_putchar('\n');
 	}
