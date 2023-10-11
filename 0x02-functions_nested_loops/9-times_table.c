c5cdb189c479a65710eeb1a108b1a56dbe00b309
mx3tez <mx3tez@gmail.com>
2023-10-11 13:11:25 +0000

    Prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,19 +9,19 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-		for (j = 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar(',');
 			result = i * j;
 			if (result >= 9)
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar((result / 10) + 48);
 				_putchar((result % 10) + 48);
 			}
 			else
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(result + 48);
