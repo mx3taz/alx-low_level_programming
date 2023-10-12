07c4c5c3fae43bf9c2f31d1ea01dd0e173f16403
mx3tez <mx3tez@gmail.com>
2023-10-12 19:56:09 +0000

    Prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,19 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar((j / 10) + 48);
-			_putchar((j % 10) + 48);
+			k = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar(k + 48);
 		}
 		_putchar('\n');
 	}
