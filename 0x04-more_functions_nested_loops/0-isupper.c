822393c2b1297eb5c95c6ce679a218b3498f84a1
mx3tez <mx3tez@gmail.com>
2023-10-12 12:01:45 +0000

    Checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ *
+ * @c: a character input
+ *
+ * Return: 1 if @c is uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 || c <= 90)
+		return (1);
+	return (0);
+}
