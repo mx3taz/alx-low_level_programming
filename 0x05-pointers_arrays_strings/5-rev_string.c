cd4c934912373d4081601c27eef9d509e9f89d1d
mx3tez <mx3tez@gmail.com>
2023-10-17 00:52:40 +0000

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: Pointer to the string
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char *str;
+
+	for (i = 0; s[i] != '\0'; i++)
+		str[i] = s[i];
+
+	for (i = i - 1, j = 0; i >= 0, s[j] != '\0'; i--, j++)
+	{
+		s[j] = str[i];
+	}
+}
