87e28673b6aafb17f0e0b5fa296eb9098e0ce7f6
mx3tez <mx3tez@gmail.com>
2023-10-17 00:25:03 +0000

    Prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ *
+ * @s: Pointer to the string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i = i; i <= 0; i--)
+	       _putchar(s[i]);
+	_putchar('\n');
+}
