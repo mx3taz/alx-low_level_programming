7680de9435fee2fcbf20d80a7fad4d5a3fdd9d48
mx3tez <mx3tez@gmail.com>
2023-10-17 01:46:04 +0000

    Prints the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,16 @@ void puts_half(char *str)
 {
 	int i, j;
 
+	// Calculate the length of the string
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
+	// Print the second half of the string
 	if (i % 2 != 0)
-		for (j = (i - 1) / 2; str[j] != '\0'; j++)
+		for (j = (i - 1) / 2; j < i; j++)
 			_putchar(str[j]);
 	else
-		for (j = i / 2; str[j] != '\0'; j++)
+		for (j = i / 2; j < i; j++)
 			_putchar(str[j]);
 	_putchar('\n');
 }
