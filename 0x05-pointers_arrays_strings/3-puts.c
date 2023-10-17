d6120aa12a79afadc4b1ca89963049f8908b7e27
mx3tez <mx3tez@gmail.com>
2023-10-17 00:10:56 +0000

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ *
+ * @str: Pointer to the string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
