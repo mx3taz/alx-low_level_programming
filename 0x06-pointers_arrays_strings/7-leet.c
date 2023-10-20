5740cfc8d0b56c58b6e8febe3ef88d519485bd08
mx3tez <mx3tez@gmail.com>
2023-10-20 23:56:49 +0000

    Encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,14 +11,14 @@ char *leet(char *s)
 {
 	char *str = s;
 	char ch1[] = {'A', 'E', 'O', 'T', 'L'};
-	char ch2[] = {4, 3, 0, 7, 1};
+	int ch2[] = {4, 3, 0, 7, 1};
 	unsigned int i;
 
-	while (*c)
+	while (*s)
 	{
 		for (i = 0; i < sizeof(ch1) / sizeof(char); i++)
 		{
-			if (*s == ch1[i] || *S == ch1[i] + 32)
+			if (*s == ch1[i] || *s == ch1[i] + 32)
 				*s = 48 + ch2[i];
 		}
 		s++;
