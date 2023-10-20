0d3b0cfb2ddfa614a8f1922d970201d9ddde43a5
mx3tez <mx3tez@gmail.com>
2023-10-20 23:16:55 +0000

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
+	i = 0;
 	while (dest[i])
 		i++;
 
-	for (j = 0; src[i]; j++)
+	for (j = 0; src[j]; j++)
 		dest[i++] = src[j];
 
 	return (dest);
