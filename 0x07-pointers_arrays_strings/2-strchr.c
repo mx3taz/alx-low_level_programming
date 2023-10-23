5023323e397e44973f3bcfa948dc27ef50f12195
mx3tez <mx3tez@gmail.com>
2023-10-23 23:59:39 +0000

    Locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: a pointer to the string
+ * @c: the character
+ *
+ * Return: return @c if the character exists,
+ * otherwise return NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+			return (c);
+	}
+	return (NULL);
+}
