61752fe5553762dcd8559079346165f08e4e6e9d
mx3tez <mx3tez@gmail.com>
2023-10-25 21:52:19 +0000

    Returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ *
+ * @s: the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s > '\0')
+		i += _strlen_recursion(s + 1) + 1;
+
+	return (i);
+}
